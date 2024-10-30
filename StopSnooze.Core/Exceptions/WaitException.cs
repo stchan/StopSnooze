d4daa12705cbda038dc7d2054c51f6463bd95330
S T Chan <1256870+stchan@users.noreply.github.com>
2024-10-30 00:38:20 -0400

    save ExitResult

--- StopSnooze.Core/Exceptions/WaitException.cs
+++ StopSnooze.Core/Exceptions/WaitException.cs
@@ -14,10 +14,14 @@ namespace StopSnooze.Core.Exceptions
     public class WaitException : StopSnoozeException
     {
         public WaitException(WaitExitResult result, string message) : base(message)
-        { }
+        {
+            ExitResult = result;
+        }
 
         public WaitException(WaitExitResult result, string message, Exception innerException) : base(message, innerException)
-        { }
+        {
+            ExitResult = result;
+        }
 
         public WaitExitResult ExitResult { get; private set; }
     }
